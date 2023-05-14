11606addc3c4309182170293c0f0b8c6b00f40bb
Sanjukta Singha <64829176+shortlisted159@users.noreply.github.com>
2023-05-14 15:04:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
   return (
     <div className="App">
       <Header onSelectChange={handleSelectChange} displayComponent={displayComponent} />
-      {/* <div>{renderSelectedComponent()}</div> */}
       <Explore/>
     </div>
   );
