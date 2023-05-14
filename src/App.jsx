33ca35994163cf9f0cab6678a0bb6e1411db8235
Sanjukta Singha <64829176+shortlisted159@users.noreply.github.com>
2023-05-14 15:04:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,6 @@ function App() {
     setDisplayComponent(event.target.value);
   }
 
-  // function renderSelectedComponent() {
-  //   switch (displayComponent) {
-  //     case "chatbot":
-  //       window.location.href = "https://www.google.com";
-  //   return null;
-  //   case "ex":
-  //     window.location.href = "https://www.nimh.nih.gov/health/topics/suicide-prevention";
-  //     return null;
-  //     case "ex1":
-  //       window.location.href = "https://sunny-blini-3f4aeb.netlify.app/";
-  //       return null;
-  //       case "ex2":
-  //         window.location.href = "https://www.thelivelovelaughfoundation.org/helpline";
-  //         return null;
-  //     default:
-        // return <Explore />;
-  //   }
-  // }
-
   return (
     <div className="App">
       <Header onSelectChange={handleSelectChange} displayComponent={displayComponent} />
