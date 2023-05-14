5b90c9b6e8bb239aa932466d8d469277cbe8043e
Sanjukta Singha <64829176+shortlisted159@users.noreply.github.com>
2023-05-14 14:56:48 +0530

    Update ChatBot.jsx

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -5,7 +5,6 @@ import { MainContainer, ChatContainer, MessageList, Message, MessageInput, Typin
 import { API_KEY } from './config.js';
 
 
-// "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
 }
