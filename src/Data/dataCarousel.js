a0e90146583d3c8be06b161b9ae0bddfbcdc74da
Sanjukta Singha <64829176+shortlisted159@users.noreply.github.com>
2023-05-17 10:47:51 +0530

    Update dataCarousel.js

--- src/Data/dataCarousel.js
+++ src/Data/dataCarousel.js
@@ -1,23 +1,23 @@
-export const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 1024 },
-    items: 1,
-    slidesToSlide: 1,
-  },
-  desktop: {
-    breakpoint: { max: 1024, min: 800 },
-    items: 1,
-  },
-  tablet: {
-    breakpoint: { max: 800, min: 464 },
-    items: 1,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
+// export const responsive = {
+//   superLargeDesktop: {
+//     // the naming can be any, depends on you.
+//     breakpoint: { max: 4000, min: 1024 },
+//     items: 1,
+//     slidesToSlide: 1,
+//   },
+//   desktop: {
+//     breakpoint: { max: 1024, min: 800 },
+//     items: 1,
+//   },
+//   tablet: {
+//     breakpoint: { max: 800, min: 464 },
+//     items: 1,
+//   },
+//   mobile: {
+//     breakpoint: { max: 464, min: 0 },
+//     items: 1,
+//   },
+// };
 
 export const productData = [
   {
